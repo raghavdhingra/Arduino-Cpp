7293f33740d28a4b17448a02ef0dd52ec69de372
Raghav Dhingra <raghav.dhingra15@gmail.com>
2021-04-23 19:47:58 +0530

    Buzzer

---
+++ BUZZER/buzzer.ino
@@ -0,0 +1,46 @@
+const int outPin = 8;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(outPin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(outPin, LOW);
+  delay(50);
+  digitalWrite(outPin, HIGH);
+  delay(50);
+  digitalWrite(outPin, LOW);
+  delay(50);
+  digitalWrite(outPin, HIGH);
+  delay(50);
+  digitalWrite(outPin, LOW);
+  delay(100);
+  digitalWrite(outPin, HIGH);
+  delay(100);
+  digitalWrite(outPin, LOW);
+  delay(200);
+  digitalWrite(outPin, HIGH);
+  delay(200);
+  digitalWrite(outPin, LOW);
+  delay(300);
+  digitalWrite(outPin, HIGH);
+  delay(300);
+  digitalWrite(outPin, LOW);
+  delay(200);
+  digitalWrite(outPin, HIGH);
+  delay(200);
+  digitalWrite(outPin, LOW);
+  delay(100);
+  digitalWrite(outPin, HIGH);
+  delay(100);
+   digitalWrite(outPin, LOW);
+  delay(50);
+  digitalWrite(outPin, HIGH);
+  delay(50);
+  digitalWrite(outPin, LOW);
+  delay(50);
+  digitalWrite(outPin, HIGH);
+  delay(50);
+}
